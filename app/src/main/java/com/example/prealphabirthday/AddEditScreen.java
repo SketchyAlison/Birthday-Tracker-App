c56ecdff65e208333b9a64bd969ce7afd441227a
SketchyAlison <alison.s.ang@gmail.com>
2019-06-08 21:48:19 -0500

    Implement btn that returns to main activity from edit screen.

--- app/src/main/java/com/example/prealphabirthday/AddEditScreen.java
+++ app/src/main/java/com/example/prealphabirthday/AddEditScreen.java
@@ -1,8 +1,12 @@
 package com.example.prealphabirthday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class AddEditScreen extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class AddEditScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_edit);
+        Button btnSave = (Button) findViewById(R.id.b_save);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Clicked Save btn.");
+                Intent saveIntent = new Intent(AddEditScreen.this, MainActivity.class);
+                startActivity(saveIntent);
+            }
+        });
     }
 }
